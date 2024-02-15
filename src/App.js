0a63c78e76290f8dcda8edb4648509c5a1347102
Leslie Lopez <leslie2515lopez@gmail.com>
2024-02-14 22:15:08 -0600

    Added loader and function to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Loader from "./components/Loader/loader.component";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  }, []);
+
+  return (
+    <Router>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          )}
+          <div className="App"></div>
+        </>
+      )}
+    </Router>
+  );
 }
-
-export default App;

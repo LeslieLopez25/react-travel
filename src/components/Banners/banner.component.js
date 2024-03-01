b5cdfc66a77a6719e64e2662cfcfd222fe70c30d
Leslie Lopez <leslie2515lopez@gmail.com>
2024-02-29 23:37:25 -0600

    Updated banner component

--- src/components/Banners/banner.component.js
+++ src/components/Banners/banner.component.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./banners.styles.css";
 
 export default function Banner() {
-  return <div className="banner-container"></div>;
+  return <div className="banner-container color"></div>;
 }

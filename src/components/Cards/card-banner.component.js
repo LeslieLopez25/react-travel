1b73573e2dfceda37bf0b1a19cd3f92987f175ea
Leslie Lopez <leslie2515lopez@gmail.com>
2024-04-07 00:01:23 -0600

    Updated card banner component

--- src/components/Cards/card-banner.component.js
+++ src/components/Cards/card-banner.component.js
@@ -5,11 +5,14 @@ import Img1 from "../../assets/images/mexico-3-min.jpg";
 export default function CardBanner() {
   return (
     <div className="card-banner">
-      <h1 className=" section-title">Discover the Unexpected</h1>
-      <CardBannerItem
-        imageUrl={Img1}
-        desc="Mexico: A land of ancient civilizations, spicy cuisine, and vibrant fiestas! From the towering pyramids of Teotihuacan to the turquoise waters of Cancun, there's never a dull moment in this colorful country!"
-      />
+      <h1 className="section-title">Discover the Unexpected</h1>
+      <div className="subtitle-container">
+        <h2 className="subtitle">Mexico</h2>
+        <CardBannerItem
+          imageUrl={Img1}
+          desc="A land of ancient civilizations, spicy cuisine, and vibrant fiestas! From the towering pyramids of Teotihuacan to the turquoise waters of Cancun, there's never a dull moment in this colorful country!"
+        />
+      </div>
     </div>
   );
 }

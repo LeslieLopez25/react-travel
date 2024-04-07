e6b17577d814ef675353f05a51a6923bb4a55b8f
Leslie Lopez <leslie2515lopez@gmail.com>
2024-04-06 23:29:27 -0600

    Added title to banner component

--- src/components/Banners/banner.component.js
+++ src/components/Banners/banner.component.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Img1 from "../../assets/images/thailand.jpg";
 import "./banners.styles.css";
 
 export default function Banner() {
-  return <div className="banner-container color"></div>;
+  const banner = [
+    {
+      url: Img1,
+      title: "Thailand",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  return (
+    <div className="banner-container color">
+      <div className="banner-overlay">
+        <h2 className="banner-title">{banner[currentIndex].title}</h2>
+      </div>
+    </div>
+  );
 }

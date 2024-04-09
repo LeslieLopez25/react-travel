d8e62038c8518e309e0f6b447bd303ae6819071c
Leslie Lopez <leslie2515lopez@gmail.com>
2024-04-08 23:38:22 -0600

    Added extra images for gallery and updated imgData file

--- src/components/pages/Gallery/imgData.js
+++ src/components/pages/Gallery/imgData.js
@@ -8,47 +8,69 @@ import Gallery7 from "../../../assets/images/japan-1-min.jpg";
 import Gallery8 from "../../../assets/images/mexico-1-min.jpg";
 import Gallery9 from "../../../assets/images/norway-1-min.jpg";
 import Gallery10 from "../../../assets/images/u.s.a-1-min.jpg";
+import Gallery11 from "../../../assets/images/germany.jpg";
+import Gallery12 from "../../../assets/images/iceland.jpg";
 
 const Gallery = [
   {
     id: 1,
     imgSrc: Gallery1,
+    title: "Canada",
   },
   {
     id: 2,
     imgSrc: Gallery2,
+    title: "England",
   },
   {
     id: 3,
     imgSrc: Gallery3,
+    title: "France",
   },
   {
     id: 4,
     imgSrc: Gallery4,
+    title: "Greece",
   },
   {
     id: 5,
     imgSrc: Gallery5,
+    title: "Ireland",
   },
   {
     id: 6,
     imgSrc: Gallery6,
+    title: "Italy",
   },
   {
     id: 7,
     imgSrc: Gallery7,
+    title: "Japan",
   },
   {
     id: 8,
     imgSrc: Gallery8,
+    title: "Mexico",
   },
   {
     id: 9,
     imgSrc: Gallery9,
+    title: "Norway",
   },
   {
     id: 10,
     imgSrc: Gallery10,
+    title: "United States",
+  },
+  {
+    id: 11,
+    imgSrc: Gallery11,
+    title: "Germany",
+  },
+  {
+    id: 12,
+    imgSrc: Gallery12,
+    title: "Iceland",
   },
 ];
 

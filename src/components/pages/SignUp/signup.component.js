fa9d088c7526d7e3da66c72e31a98b17468dc186
Leslie Lopez <leslie2515lopez@gmail.com>
2024-04-17 00:16:58 -0600

    Working on ui for sign up component and css

--- src/components/pages/SignUp/signup.component.js
+++ src/components/pages/SignUp/signup.component.js
@@ -5,7 +5,17 @@ import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 export default function SignUp() {
   return (
     <div>
-      <div></div>
+      <div className="signup-container">
+        <h1 className="section-title">Sign Up</h1>
+        <p className="signup-error">Error</p>
+        <form className="signup-form_container">
+          <label>Email</label>
+          <div className="signup-input_container">
+            <input type="email" className="signup-input" />
+            <FontAwesomeIcon icon={faEnvelope} />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }

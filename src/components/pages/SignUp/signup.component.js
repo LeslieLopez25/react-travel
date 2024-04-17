4ef6dc874dc0cc0f8ee825f7c1222ee2018c4b09
Leslie Lopez <leslie2515lopez@gmail.com>
2024-04-16 23:55:43 -0600

    Setting up the ui for signup component

---
+++ src/components/pages/SignUp/signup.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+
+export default function SignUp() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}

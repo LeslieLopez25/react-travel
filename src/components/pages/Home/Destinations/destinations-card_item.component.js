659bcec961a6b5382b322c462e67a10b3e01ec5c
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-27 19:51:43 -0600

    Removed fontawesome npm package

--- src/components/pages/Home/Destinations/destinations-card_item.component.js
+++ src/components/pages/Home/Destinations/destinations-card_item.component.js
@@ -1,12 +1,7 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
-import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
+import React from "react";
 
 // DestinationsCardItem component for the Card component
 export default function DestinationsCardItem({ label, src, desc }) {
-  const [savedDestination, setSavedDestination] = useState(false);
-
   return (
     <li className="destinations-cards__item">
       <div className="destinations-cards__item__block">
@@ -14,15 +9,6 @@ export default function DestinationsCardItem({ label, src, desc }) {
           className="destinations-cards__item__pic-wrap"
           data-category={label}
         >
-          <div className="heart-icons">
-            <div className="heart-icon_box">
-              {savedDestination ? (
-                <FontAwesomeIcon icon={solidHeart} className="heart-icon" />
-              ) : (
-                <FontAwesomeIcon icon={regularHeart} className="heart-icon" />
-              )}
-            </div>
-          </div>
           <img
             className="destinations-cards__item__img"
             src={src}
